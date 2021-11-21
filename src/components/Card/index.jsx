f6638f6817c5103e9eb74edf028e72ff73c52b96
RomanAnpilov <rebokuter@gmail.com>
2021-11-21 14:36:41 +0300

    Initialize project

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Card.module.scss'
+
+const Card = () => {
+    return (
+        <div className={styles.Card}> CARD</div>
+    )
+}
+
+export default Card;
---
+++ src/components/Panel/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Panel.module.scss'
+
+const Panel = () => {
+    return (
+        <div className={styles.panel}></div>
+    )
+}
+
+export default Panel;
